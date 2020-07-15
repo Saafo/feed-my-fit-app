710879d523ce5cceaa13f5cc56e60f5dedb8aa90
Saafo <saffordh@163.com>
2020-07-15 18:05:07 +0800

    Initial Commit

---
+++ FeedMyFit/FeedMyFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedMyFitApp.swift
+//  FeedMyFit
+//
+//  Created by mintsky on 2020/7/15.
+//
+
+import SwiftUI
+
+@main
+struct FeedMyFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
