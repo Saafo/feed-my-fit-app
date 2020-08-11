2d84f336e4ef420f217af77a11acb1f175d6d820
ATalkingDog <57002479+ATalkingDog@users.noreply.github.com>
2020-08-11 17:42:44 +0800

    Ini The main Pages

--- FeedMyFit/Models/Publicinfo.swift
+++ FeedMyFit/Models/Publicinfo.swift
@@ -22,3 +22,24 @@ struct Publicinfo: Codable {
     var City: String
     var Streak: Int
 }
+//
+//struct Static {
+//    var date: String
+//    struct date {
+//        var dt1: String
+//        var dt2: String
+//    }
+//}
+//
+//var statics: [Static] = []
+//
+//var finalDict: [[String: String]] = []
+//
+//func initStatics() {
+//    let dict = ["2020-01-01": ["dt1":"vl1","dt2":"vl2"], "2020-04-04": ["dt1":"vl1","dt2":"vl2"], "2020-06-06": ["dt1":"vl1","dt2":"vl2"]]
+//    //temp = [["date""],xxxxxx]
+//    for date in dict.keys {
+////        statics.append(Static(date: date, dt1: dict[date]!["dt1"]!, dt2: dict[date]!["dt2"]!))
+//        finalDict.append(["date": dict[date]])
+//    }
+//}
