430d6d4fac823a961a3ca56b9a59ddf702398122
ATalkingDog <57002479+ATalkingDog@users.noreply.github.com>
2020-08-13 19:17:01 +0800

    Fix: some bugs of Live2D supporting

--- FeedMyFit/Utilities/Live2DHelper.h
+++ FeedMyFit/Utilities/Live2DHelper.h
@@ -5,12 +5,12 @@
 //  Created by 祁汝鑫 on 2020/8/13.
 //
 
-#import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-
-@interface Live2dHelper : NSObject
-- (void)onTick:(NSTimeInterval)duration;
-@end
-
-
-NS_ASSUME_NONNULL_END
+//#import <UIKit/UIKit.h>
+//#import <Foundation/Foundation.h>
+//
+//@interface Live2dHelper : NSObject
+//- (void)onTick:(NSTimeInterval)duration;
+//@end
+//
+//
+//NS_ASSUME_NONNULL_END
--- FeedMyFit/Utilities/Live2DViewController.swift
+++ FeedMyFit/Utilities/Live2DViewController.swift
@@ -4,45 +4,45 @@
 //
 //  Created by 祁汝鑫 on 2020/8/13.
 //
-
-import UIKit
-
-final class Live2DViewController: UIViewController {
-    
-    let cameraController = CameraController()
-    var previewView: UIView!
-    
-    override func viewDidLoad() {
-                
-        previewView = UIView(frame: CGRect(x:0, y:0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
-        previewView.contentMode = UIView.ContentMode.scaleAspectFit
-        view.addSubview(previewView)
-        
-        cameraController.prepare {(error) in
-            if let error = error {
-                print(error)
-            }
-            
-            try? self.cameraController.displayPreview(on: self.previewView)
-        }
-        
-    }
-    
-    func glkView(_ view: GLKView,
-                 drawInrect: CGRect) -> () {
-        <#function body#>
-    }
-    
-}
-
-extension Live2DViewController : UIViewControllerRepresentable{
-    public typealias UIViewControllerType = Live2DViewController
-    
-    public func makeUIViewController(context: UIViewControllerRepresentableContext<Live2DViewController>) -> Live2DViewController {
-        return Live2DViewController()
-    }
-    
-    public func updateUIViewController(_ uiViewController: Live2DViewController, context: UIViewControllerRepresentableContext<Live2DViewController>) {
-    }
-}
-
+//
+//import UIKit
+//
+//final class Live2DViewController: UIViewController {
+//    
+//    let cameraController = CameraController()
+//    var previewView: UIView!
+//    
+//    override func viewDidLoad() {
+//                
+//        previewView = UIView(frame: CGRect(x:0, y:0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
+//        previewView.contentMode = UIView.ContentMode.scaleAspectFit
+//        view.addSubview(previewView)
+//        
+//        cameraController.prepare {(error) in
+//            if let error = error {
+//                print(error)
+//            }
+//            
+//            try? self.cameraController.displayPreview(on: self.previewView)
+//        }
+//        
+//    }
+//    
+//    func glkView(_ view: GLKView,
+//                 drawInrect: CGRect) -> () {
+//        <#function body#>
+//    }
+//    
+//}
+//
+//extension Live2DViewController : UIViewControllerRepresentable{
+//    public typealias UIViewControllerType = Live2DViewController
+//    
+//    public func makeUIViewController(context: UIViewControllerRepresentableContext<Live2DViewController>) -> Live2DViewController {
+//        return Live2DViewController()
+//    }
+//    
+//    public func updateUIViewController(_ uiViewController: Live2DViewController, context: UIViewControllerRepresentableContext<Live2DViewController>) {
+//    }
+//}
+//
